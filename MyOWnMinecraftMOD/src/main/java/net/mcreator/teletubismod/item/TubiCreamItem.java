aa9092e45571388d7d23c10749b56b98cdb35314
Loyydowski <barto.kub@gmail.com>
2026-02-26 19:10:39 +0100

    v.0.0.1
    Not working eating

---
+++ MyOWnMinecraftMOD/src/main/java/net/mcreator/teletubismod/item/TubiCreamItem.java
@@ -0,0 +1,41 @@
+package net.mcreator.teletubismod.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+
+public class TubiCreamItem extends Item {
+	public TubiCreamItem() {
+		super(new Item.Properties().food((new FoodProperties.Builder()).nutrition(6).saturationMod(0.5f).build()));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.NONE;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 0;
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = new ItemStack(Items.BOWL);
+		super.finishUsingItem(itemstack, world, entity);
+		if (itemstack.isEmpty()) {
+			return retval;
+		} else {
+			if (entity instanceof Player player && !player.getAbilities().instabuild) {
+				if (!player.getInventory().add(retval))
+					player.drop(retval, false);
+			}
+			return itemstack;
+		}
+	}
+}
